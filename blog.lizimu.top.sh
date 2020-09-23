4f68d397ca0b90486b3607983e9f16dcb0edb9d7
lizhigao <lizhigao@021.com>
2020-09-23 19:27:36 +0800

    update

--- blog.lizimu.top.sh
+++ blog.lizimu.top.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-
-scp -r ./public/* ubuntu@94.191.117.102:/data/www/blog
+SSH_KEY_PATH="/Users/admin/.ssh/mbpwork"
+ASYNC_PATH="./public/*"
+DEST_SERVER_PATH="ubuntu@94.191.117.102:/data/www/blog"
+# SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+scp -i ${SSH_KEY_PATH} -r ${ASYNC_PATH} ${DEST_SERVER_PATH}
+# rsync -a -e `ssh -i ${SSH_KEY_PATH}` ${ASYNC_PATH} ${DEST_SERVER_PATH}
---
+++ source/about/index.md
@@ -0,0 +1,8 @@
+---
+title: 关于
+date: 2020-09-23 18:32:30
+---
+
+- [iris](/website/iris/about_me.html)
+- [iris 作品集](/website/iris/album.html)
+- [cleam's weui project](/website/cleam/weui_pro/index.html)
